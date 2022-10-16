1b1c41d4c6e2541f41f46e2b9cc75684e1f64c5b
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2022-10-17 00:35:31 +0600

    not found page added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Cart from './components/Cart';
 import Home from './components/Home';
 import NavBar from './components/NavBar';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/cart' element={<Cart />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
       </Routes>
     </div>
   );
