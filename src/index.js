7be0012e8dd7db0c491cfc2466e6b34c54c0788d
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2022-10-29 13:36:11 +0600

    rtk query

--- src/index.js
+++ src/index.js
@@ -4,14 +4,31 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import productsReducer, { productsFetch } from "./features/productsSlice";
+import { productsApi } from './features/productsApi';
+
+
+const store = configureStore({
+  reducer: {
+    products: productsReducer,
+    [productsApi.reducerPath]: productsApi.reducer,
+  },
+  middleware:(getDefaultMiddleware)=>{
+    getDefaultMiddleware().concat(productsApi.middleware);
+  },
+});
+
+store.dispatch(productsFetch());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
       <App />
-    </React.StrictMode>
-  </BrowserRouter>
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
